82d08037b3569aae80b1b934fb1772c31aaead0d
GuangForGithub <guangms@hotmail.com>
2022-09-13 05:23:19 +0800

    学习第4单元，并改用mysql数据库

---
+++ Data/PizzaContext.cs
@@ -0,0 +1,17 @@
+﻿//using BlazingPizza.Model;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace BlazingPizza.Data
+//{
+//    public class PizzaContext : DbContext
+//    {
+//        public PizzaContext(DbContextOptions<PizzaContext> options)
+//            : base(options)
+//        {
+//        }
+
+//        public DbSet<Pizza> Pizzas {get;set;}
+//        //public DbSet<Topping> Toppings => Set<Topping>();
+//        //public DbSet<Sauce> Sauces => Set<Sauce>();
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
+using BlazingPizza.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+//将自定义 DbContext（此处是CarDbContext） 添加到 DI 容器
+//DI容器是ASP.NET Core 应用程序服务提供程序，也称为依赖关系注入容器
+builder.Services.AddDbContext<PizzaStoreContext>(opt => opt.UseMySql(
+		builder.Configuration.GetConnectionString("MySqlConnection"),
+		new MySqlServerVersion(new Version("8.0.30"))));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+// Register the pizzas service
+//builder.Services.AddSingleton<PizzaService>();
+
+//添加服务
+builder.Services.AddScoped<PizzaService>();
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+	app.UseExceptionHandler("/Error");
 }
 
 app.UseStaticFiles();
@@ -16,5 +33,22 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+	var psdb = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+	if (psdb.Database.EnsureCreated())
+	{
+		SeedData.InitializePizzaSpecial(psdb);
+		//if (!psdb.Pizzas.Any())
+		//{
+		//    SeedData.InitializePizza(psdb);
+		//}
+	}
+}
 
 app.Run();
